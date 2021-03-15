b76ec061759201c923e91f539b0704fb1e000383
valeros <valeros08@gmail.com>
2021-03-15 12:50:29 +0200

    Add an explicit dependency on checkprogsize target for the final binary

--- builder/main.py
+++ builder/main.py
@@ -231,6 +231,7 @@ else:
     else:
         target_firm = env.ElfToBin(
             join("$BUILD_DIR", "${PROGNAME}"), target_elf)
+        env.Depends(target_firm, "checkprogsize")
 
 env.AddPlatformTarget("buildfs", target_firm, target_firm, "Build Filesystem Image")
 AlwaysBuild(env.Alias("nobuild", target_firm))
