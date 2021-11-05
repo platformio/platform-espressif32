f7bcd7e692f87c12a8359e32d0159246d8187a19
valeros <valeros08@gmail.com>
2021-11-05 14:48:05 +0200

    Revert "Use default flash mode when uploading via esptoolpy"
    Resolve #584, resolve #630, resolve #623
    
    This reverts commit fe5d2796d46cc4fb29f8a77cbfa66f53faf4fb89.

--- builder/main.py
+++ builder/main.py
@@ -31,6 +31,15 @@ def _get_board_f_flash(env):
     return str(int(int(frequency) / 1000000)) + "m"
 
 
+def _get_board_flash_mode(env):
+    mode = env.subst("$BOARD_FLASH_MODE")
+    if mode == "qio":
+        return "dio"
+    elif mode == "qout":
+        return "dout"
+    return mode
+
+
 def _parse_size(value):
     if isinstance(value, int):
         return value
@@ -135,6 +144,7 @@ if mcu == "esp32c3":
 
 env.Replace(
     __get_board_f_flash=_get_board_f_flash,
+    __get_board_flash_mode=_get_board_flash_mode,
 
     AR="%s-elf-ar" % toolchain_arch,
     AS="%s-elf-as" % toolchain_arch,
@@ -307,7 +317,7 @@ elif upload_protocol == "esptool":
             "--before", "default_reset",
             "--after", "hard_reset",
             "write_flash", "-z",
-            "--flash_mode", "$BOARD_FLASH_MODE",
+            "--flash_mode", "${__get_board_flash_mode(__env__)}",
             "--flash_freq", "${__get_board_f_flash(__env__)}",
             "--flash_size", "detect"
         ],
