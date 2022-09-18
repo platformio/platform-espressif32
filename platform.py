c869c06cf996eb550272ddd50af41fcd16e0a716
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2022-09-18 17:55:17 +0200

    Update platform.py

--- platform.py
+++ platform.py
@@ -263,7 +263,7 @@ class Espressif32Platform(PlatformBase):
 
         if "openocd" in (debug_config.server or {}).get("executable", ""):
             debug_config.server["arguments"].extend(
-                ["-c", "adapter speed %s" % (debug_config.speed or "5000")]
+                ["-c", "adapter speed %s" % (debug_config.speed or "20000")]
             )
 
         ignore_conds = [
