74339291cf66f4520dd2d178b1e8e1346cac6f2c
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2023-01-06 13:57:47 +0100

    Take into account board flash mode when converting elf to bin

--- builder/main.py
+++ builder/main.py
@@ -273,7 +273,7 @@ env.Append(
                 '"$PYTHONEXE" "$OBJCOPY"',
                 "--chip", mcu, "elf2image",
                 "--dont-append-digest",
-                "--flash_mode", "$BOARD_FLASH_MODE",
+                "--flash_mode", "${__get_board_flash_mode(__env__)}",
                 "--flash_freq", "${__get_board_f_flash(__env__)}",
                 "--flash_size", board.get("upload.flash_size", "4MB"),
                 "-o", "$TARGET", "$SOURCES"
