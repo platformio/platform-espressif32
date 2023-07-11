5be00758fbe9e2a78a4de4d06db3484bea3e4435
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2023-07-11 20:42:30 +0200

    USE ADC example

--- examples/espidf-ulp-riscv/main/ulp_riscv_example_main.c
+++ examples/espidf-ulp-riscv/main/ulp_riscv_example_main.c
@@ -1,4 +1,4 @@
-/* ULP riscv example
+/* ULP riscv DS18B20 1wire temperature sensor example
 
    This example code is in the Public Domain (or CC0 licensed, at your option.)
 
@@ -12,18 +12,12 @@
 #include "soc/rtc_cntl_reg.h"
 #include "soc/sens_reg.h"
 #include "soc/rtc_periph.h"
-#include "hal/rtc_io_ll.h"
 #include "driver/gpio.h"
 #include "driver/rtc_io.h"
 #include "ulp_riscv.h"
 #include "ulp_main.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-
-/* We alternate between start conversion and read result every other ULP wakeup,
-   Conversion time is 750 ms for 12 bit resolution
-*/
-#define WAKEUP_PERIOD_US (750000)
 
 extern const uint8_t ulp_main_bin_start[] asm("_binary_ulp_main_bin_start");
 extern const uint8_t ulp_main_bin_end[]   asm("_binary_ulp_main_bin_end");
@@ -32,18 +26,28 @@ static void init_ulp_program(void);
 
 void app_main(void)
 {
+    /* Initialize selected GPIO as RTC IO, enable input, disable pullup and pulldown */
+    rtc_gpio_init(GPIO_NUM_0);
+    rtc_gpio_set_direction(GPIO_NUM_0, RTC_GPIO_MODE_INPUT_ONLY);
+    rtc_gpio_pulldown_dis(GPIO_NUM_0);
+    rtc_gpio_pullup_dis(GPIO_NUM_0);
+    rtc_gpio_hold_en(GPIO_NUM_0);
+
     esp_sleep_wakeup_cause_t cause = esp_sleep_get_wakeup_cause();
     /* not a wakeup from ULP, load the firmware */
     if (cause != ESP_SLEEP_WAKEUP_ULP) {
-        printf("Not a ULP-RISC-V wakeup (cause = %d), initializing it! \n", cause);
+        printf("Not a ULP-RISC-V wakeup, initializing it! \n");
         init_ulp_program();
     }
 
-    /* ULP Risc-V read and detected a temperature above the limit */
+    /* ULP Risc-V read and detected a change in GPIO_0, prints */
     if (cause == ESP_SLEEP_WAKEUP_ULP) {
-        printf("ULP-RISC-V woke up the main CPU, temperature is above set limit! \n");
-        printf("ULP-RISC-V read temperature is %f\n", ulp_temp_reg_val / 16.0);
+        printf("ULP-RISC-V woke up the main CPU! \n");
+        printf("ULP-RISC-V read changes in GPIO_0 current is: %s \n",
+            (bool)(ulp_gpio_level_previous == 0) ? "Low" : "High" );
+
     }
+
     /* Go back to sleep, only the ULP Risc-V will run */
     printf("Entering in deep sleep\n\n");
 
@@ -51,7 +55,6 @@ void app_main(void)
     vTaskDelay(100);
 
     ESP_ERROR_CHECK( esp_sleep_enable_ulp_wakeup());
-
     esp_deep_sleep_start();
 }
 
@@ -61,9 +64,9 @@ static void init_ulp_program(void)
     ESP_ERROR_CHECK(err);
 
     /* The first argument is the period index, which is not used by the ULP-RISC-V timer
-     * The second argument is the period in microseconds, which gives a wakeup time period of: 750ms
+     * The second argument is the period in microseconds, which gives a wakeup time period of: 20ms
      */
-    ulp_set_wakeup_period(0, WAKEUP_PERIOD_US);
+    ulp_set_wakeup_period(0, 20000);
 
     /* Start the program */
     err = ulp_riscv_run();
