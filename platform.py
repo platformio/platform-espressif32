b7e9fce1921369240504fccb4a3b8dff10987589
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2023-07-22 16:00:20 +0200

    initial c2 support

--- platform.py
+++ platform.py
@@ -81,8 +81,8 @@ class Espressif32Platform(PlatformBase):
             else:
                 self.packages.pop("toolchain-xtensa-%s" % available_mcu, None)
 
-        if mcu in ("esp32s2", "esp32s3", "esp32c3", "esp32c6", "esp32h2"):
-            if mcu in ("esp32c3", "esp32c6", "esp32h2"):
+        if mcu in ("esp32s2", "esp32s3", "esp32c2", "esp32c3", "esp32c6", "esp32h2"):
+            if mcu in ("esp32c2", "esp32c3", "esp32c6", "esp32h2"):
                 self.packages.pop("toolchain-esp32ulp", None)
             # RISC-V based toolchain for ESP32C3, ESP32C6 ESP32S2, ESP32S3 ULP
             self.packages["toolchain-riscv32-esp"]["optional"] = False
@@ -124,7 +124,7 @@ class Espressif32Platform(PlatformBase):
             "tumpa",
         ]
 
-        if board.get("build.mcu", "") in ("esp32c3", "esp32c6", "esp32s3", "esp32h2"):
+        if board.get("build.mcu", "") in ("esp32c2", "esp32c3", "esp32c6", "esp32s3", "esp32h2"):
             supported_debug_tools.append("esp-builtin")
 
         upload_protocol = board.manifest.get("upload", {}).get("protocol")
