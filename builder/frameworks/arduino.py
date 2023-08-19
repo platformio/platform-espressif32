9c620566e9d3d14fd97dbc1dba15dbdcde8c6939
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2023-08-19 20:50:50 +0200

    v0.4.3

--- builder/frameworks/arduino.py
+++ builder/frameworks/arduino.py
@@ -79,7 +79,7 @@ def install_python_deps():
     deps = {
         "wheel": ">=0.35.1",
         "zopfli": ">=0.2.2",
-        "tasmota-metrics": ">=0.4.2"
+        "tasmota-metrics": ">=0.4.3"
     }
 
     installed_packages = _get_installed_pip_packages()
