f0d95856ce73b0423ffc48a343c2feb09fa9362b
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-01-20 13:18:50 +0100

    Delete examples/espidf-arduino-littlefs/littlefsbuilder.py

--- examples/espidf-arduino-littlefs/littlefsbuilder.py
+++
@@ -1,3 +0,0 @@
-Import("env")
-platform = env.PioPlatform()
-env.Replace( MKSPIFFSTOOL=platform.get_package_dir("tool-mklittlefs") + '/mklittlefs' )  # PlatformIO now believes it has actually created a SPIFFS
