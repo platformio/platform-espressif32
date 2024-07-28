cebd6ee8c51a21eabe9cebb761fdd1becd856354
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-07-28 23:25:48 +0200

    Update platform.py

--- platform.py
+++ platform.py
@@ -103,7 +103,7 @@ class Espressif32Platform(PlatformBase):
                     self.packages["toolchain-xtensa-esp-elf"]["version"] = Espressif32Platform.xtensa_toolchain[sys_type]
 
             if mcu in ("esp32s2", "esp32s3", "esp32c2", "esp32c3", "esp32c6", "esp32h2"):
-                if mcu in ("esp32c2", "esp32c3", "esp32c6", "esp32h2"):
+                if mcu in ("esp32c2", "esp32c3", "esp32h2"):
                     self.packages.pop("toolchain-esp32ulp", None)
                 # RISC-V based toolchain for ESP32C3, ESP32C6 ESP32S2, ESP32S3 ULP
                 self.packages["toolchain-riscv32-esp"]["optional"] = False
