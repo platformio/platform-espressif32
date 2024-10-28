14a735c4eb08551aa0f1fff9aa9b67c289eaa844
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-10-28 13:44:32 +0100

    three frameworks and hybrid compile together

--- builder/frameworks/arduino.py
+++ builder/frameworks/arduino.py
@@ -36,19 +36,27 @@ from platformio.project.config import ProjectConfig
 from platformio.package.manager.tool import ToolPackageManager
 
 env = DefaultEnvironment()
+pm = ToolPackageManager()
 platform = env.PioPlatform()
 config = env.GetProjectConfig()
 board = env.BoardConfig()
 mcu = board.get("build.mcu", "esp32")
 flag_custom_sdkconfig = config.has_option("env:"+env["PIOENV"], "custom_sdkconfig")
 extra_flags = (''.join([element for element in board.get("build.extra_flags", "")])).replace("-D", " ")
+build_flags = ''.join([element.replace("-D", " ") for element in env.GetProjectOption("build_flags")])
 framework_reinstall = False
 
-pm = ToolPackageManager()
+if ("CORE32SOLO1" in extra_flags or "FRAMEWORK_ARDUINO_SOLO1" in build_flags) and ("arduino" in env.subst("$PIOFRAMEWORK")) and flag_custom_sdkconfig == False:
+    FRAMEWORK_DIR = platform.get_package_dir("framework-arduino-solo1")
+elif ("CORE32ITEAD" in extra_flags or "FRAMEWORK_ARDUINO_ITEAD" in build_flags) and ("arduino" in env.subst("$PIOFRAMEWORK")) and flag_custom_sdkconfig == False:
+    FRAMEWORK_DIR = platform.get_package_dir("framework-arduino-ITEAD")
+elif "arduino" in env.subst("$PIOFRAMEWORK") and "CORE32SOLO1" not in extra_flags and "FRAMEWORK_ARDUINO_SOLO1" not in build_flags and "CORE32ITEAD" not in extra_flags and "FRAMEWORK_ARDUINO_ITEAD" not in build_flags:
+    FRAMEWORK_DIR = platform.get_package_dir("framework-arduinoespressif32")
+elif "arduino" in env.subst("$PIOFRAMEWORK") and flag_custom_sdkconfig == True:
+    FRAMEWORK_DIR = platform.get_package_dir("framework-arduinoespressif32")
 
 SConscript("_embed_files.py", exports="env")
 
-FRAMEWORK_DIR = platform.get_package_dir("framework-arduinoespressif32")
 flag_any_custom_sdkconfig = os.path.exists(join(FRAMEWORK_DIR,"tools","esp32-arduino-libs","sdkconfig"))
 
 # Esp32-solo1 libs needs adopted settings
