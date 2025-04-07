8cb4e5dffcd41f74235033f87f7bb2993a1db9eb
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2025-04-07 21:02:38 +0200

    Update main.py

--- builder/main.py
+++ builder/main.py
@@ -305,8 +305,20 @@ env.Replace(
     PROGSUFFIX=".elf"
 )
 
-# Set lib_archive to False for all envs to avoid issues with weak defs in framework and libs
-config.set("env:" + env["PIOENV"], "lib_archive", "False")
+# Check if lib_archive is set in platformio.ini and set it to False
+# if not found. This makes weak defs in framework and libs possible.
+def check_lib_archive_exists():
+    for section in config.sections():
+        if "lib_archive" in config.options(section):
+            #print(f"lib_archive in [{section}] found with value: {config.get(section, 'lib_archive')}")
+            return True
+    #print("lib_archive was not found in platformio.ini")
+    return False
+
+if not check_lib_archive_exists():
+    env_section = "env:" + env["PIOENV"]
+    config.set(env_section, "lib_archive", "False")
+    #print(f"lib_archive is set to False in [{env_section}]")
 
 # Allow user to override via pre:script
 if env.get("PROGNAME", "program") == "program":
