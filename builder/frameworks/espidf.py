a58aedcf2977bb2bd8e26f58cb345ef378884a52
valeros <valeros08@gmail.com>
2026-08-31 13:58:04 +0300

    Bump cryptography to v46 for local IDF venv
    This version has prebuilt wheels for win_arm64

--- builder/frameworks/espidf.py
+++ builder/frameworks/espidf.py
@@ -1510,7 +1510,7 @@ def install_python_deps():
         # https://github.com/platformio/platformio-core/issues/4614
         "urllib3": "<2",
         # https://github.com/platformio/platform-espressif32/issues/635
-        "cryptography": "~=44.0.0" if IDF5_OR_NEWER else ">=2.1.4,<35.0.0",
+        "cryptography": "~=46.0.0" if IDF5_OR_NEWER else ">=2.1.4,<35.0.0",
         "pyparsing": ">=3.1.0,<4" if IDF5_OR_NEWER else ">=2.0.3,<2.4.0",
         "idf-component-manager": "~=3.1" if IDF5_OR_NEWER else "~=1.0",
         "esp-idf-kconfig": "~=3.6.0",
